ee80edc33fc9e73a22066147ecd5159859f7fdb3
Stanislav Verovkin <sverevkin25@gmail.com>
2018-12-16 20:40:40 +0200

    first

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ $(function () {
 
     $.scrollIt({
 
-        easing: 'swing',      // the easing function for animation
-        scrollTime: 900,       // how long (in ms) the animation takes
-        activeClass: 'active', // class given to the active nav element
-        onPageChange: null,    // function(pageIndex) that is called when page is changed
-        topOffset: -70,         // offste (in px) for fixed top navigation
-        upKey: 38,                // key code to navigate to the next section
-        downKey: 40          // key code to navigate to the previous section
+        easing: 'swing',
+        scrollTime: 900,
+        activeClass: 'active',
+        onPageChange: null,
+        topOffset: -70,
+        upKey: 38,
+        downKey: 40
 
     });
 
-    var win = $(window);
+    let win = $(window);
 
 
     win.on("scroll", function () {
-        var wScrollTop  = $(window).scrollTop();
+        let wScrollTop = $(window).scrollTop();
 
         if (wScrollTop > 150) {
             $(".navbar").addClass("navbar-colored");
@@ -27,7 +27,6 @@ $(function () {
         }
     });
 
-    /* close navbar-collapse when a  clicked */
     $(".navbar-nav a").on('click', function () {
         $(".navbar-collapse").removeClass("show");
     });
@@ -36,8 +35,7 @@ $(function () {
         $(this).toggleClass('bars-rotate');
     });
 
-    /* scroll-top-btn */
-    var  scrollButton = $('.scroll-top-btn .fa');
+    let scrollButton = $('.scroll-top-btn .fa');
     win.on('scroll', function () {
         if ($(this).scrollTop() >= 700) {
             scrollButton.show();
@@ -46,59 +44,37 @@ $(function () {
         }
     });
 
-    /* Click On scroll-top-btn */
     scrollButton.on('click', function () {
-        $('html,body').animate({ scrollTop : 0 }, 1200);
+        $('html,body').animate({scrollTop: 0}, 1200);
     });
 
-    /* wow */
-    new WOW().init();
-
-    /*  skills-area section  */
     win.on('scroll', function () {
         $(".skills-progress span").each(function () {
-            var bottom_of_object =
+            let bottom_of_object =
                 $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window =
+            let bottom_of_window =
                 $(window).scrollTop() + $(window).height();
-            var myVal = $(this).attr('data-value');
-            if(bottom_of_window > bottom_of_object) {
+            let myVal = $(this).attr('data-value');
+            if (bottom_of_window > bottom_of_object) {
                 $(this).css({
-                    width : myVal
+                    width: myVal
                 });
             }
         });
     });
 });
 
+$(window).on("load", function () {
 
-
-$(window).on("load",function (){
-
-    $('.load-wrapp').fadeOut(100);
-
-    /* isotope */
     $('.grid').isotope({
-        // options
         itemSelector: '.items'
     });
+});
 
-    var $grid = $('.grid').isotope({
-        // options
-    });
-
-    // filter items on button click
-    $('.filtering').on( 'click', 'span', function() {
-
-        var filterValue = $(this).attr('data-filter');
-
-        $grid.isotope({ filter: filterValue });
-
-    });
-
-    $('.filtering').on( 'click', 'span', function() {
-
-        $(this).addClass('active').siblings().removeClass('active');
-
-    });
+$('.work-image > a').magnificPopup({
+    removalDelay: 300,
+    type: 'image',
+    gallery: {
+        enabled: true
+    }
 });
